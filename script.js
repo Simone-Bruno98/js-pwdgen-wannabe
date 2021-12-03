d2f5cdf05f9f7ea3a8acd9331157b506ae0f6750
Simone-Bruno98 <bruno-simone@outlook.it>
2021-12-03 09:47:52 +0100

    Es sistemato e finito

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 // variabili
 
 const year = "2021";
-let name= prompt("Please write your name here", "Name")
-let surname= prompt("Please write your surname here", "Surname")
-let favouritecolor= prompt("Please write here your favourite color", "Favourite Color")
+let name= prompt("Please write your name here", "Name");
+let surname= prompt("Please write your surname here", "Surname");
+let favouritecolor= prompt("Please write here your favourite color", "FavouriteColor");
 
-password = name+surname+favouritecolor+year
+password = name+surname+favouritecolor+year;
 
-console.log(password)
-console.log(
-    `
-    ${name} ${surname} ${favouritecolor}
-    asdasdasd
-    `
-)
-password = document.getElementById("s").innerHTML
-document.getElementById('generatedpassword').innerHTML = 'password';
+console.log(password);
+
+document.writeln(password);
